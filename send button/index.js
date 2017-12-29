9f2a8748a4e078bd378caaf33d994a3d6e4a9bbb
Priyanka Malviya <priyanka_malviya@icloud.com>
2017-12-28 18:38:05 -0800

    Create index.js

---
+++ send button/index.js
@@ -0,0 +1,17 @@
+
+document.querySelector('button').addEventListener('click',function clickHandler(e){
+
+    this.removeEventListener('click',clickHandler,false);
+
+    e.preventDefault();
+    var self = this;
+    setTimeout(function(){
+        self.className = 'loading';
+    },125);
+
+    setTimeout(function(){
+        self.className = 'sent!';
+    },4300);
+
+},false);
+
